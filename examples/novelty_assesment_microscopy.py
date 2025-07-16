4087378661eb6b358537c8210593fe3cbe9423c2
Maxim Ziatdinov <ziatdinovmax@gmail.com>
2025-07-16 14:18:16 -0700

    Update examples

---
+++ examples/novelty_assesment_microscopy.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import sys
+sys.path.append("../scilink")
+
+import scilink
+
+# Configure APIs
+scilink.configure('google', '')
+scilink.configure('futurehouse', '')
+
+
+# Define image and metadata
+image_path = "examples/data/MoS2_stem_Exp-001-5.npy" # GO_cafm.tif, nanoparticles.npy
+system_info_path = "examples/data/MoS2_stem_Exp-001-5.json" # GO_cafm.json, nanoparticles.json
+
+if __name__ == "__main__":
+
+    workflow = scilink.workflows.ExperimentNoveltyAssessment(
+        data_type='microscopy',
+        dft_recommendations=True,
+        enable_human_feedback=True
+    )
+
+    result_unified = workflow.run_complete_workflow(
+        data_path=image_path,
+        system_info=system_info_path
+    )
+
+    print("\n--- Workflow Summary ---")
+    print(workflow.get_summary(result_unified))
