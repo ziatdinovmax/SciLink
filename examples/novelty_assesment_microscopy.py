9854621aac745aa44fa80655051359b0f5711c39
Maxim Ziatdinov <ziatdinovmax@gmail.com>
2025-07-28 08:24:51 -0700

    Update example

--- examples/novelty_assesment_microscopy.py
+++ examples/novelty_assesment_microscopy.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python3
 
 import sys
-sys.path.append("../scilink")
+sys.path.append("../../scilink")
 
 import scilink
 
 # Configure APIs
 scilink.configure('google', '')
-scilink.configure('futurehouse','')
+scilink.configure('futurehouse', '')
 
 
 # Define image and metadata
-image_path = "data/MoS2_stem_Exp-000-2.npy" # GO_cafm.tif, nanoparticles.npy
-system_info_path = "data/MoS2_stem_Exp-000-2.json" # GO_cafm.json, nanoparticles.json
+image_path = "data/MoS2_stem_Exp-001-5.npy" # GO_cafm.tif, nanoparticles.npy
+system_info_path = "data/MoS2_stem_Exp-001-5.json" # GO_cafm.json, nanoparticles.json
 
 if __name__ == "__main__":
 
     workflow = scilink.workflows.ExperimentNoveltyAssessment(
         data_type='microscopy',
-        dft_recommendations=True,
         enable_human_feedback=True,
+        dft_recommendations=True,
+        measurement_recommendations=True
     )
 
     result_unified = workflow.run_complete_workflow(
